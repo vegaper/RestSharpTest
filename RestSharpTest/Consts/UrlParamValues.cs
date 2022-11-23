40986bdaa0fef23304af6c16d29898cba54c5929
vegaper <39676491+vegaper@users.noreply.github.com>
2022-11-23 18:32:11 +0200

    Update UrlParamValues.cs
    removed token

--- RestSharpTest/Consts/UrlParamValues.cs
+++ RestSharpTest/Consts/UrlParamValues.cs
@@ -11,8 +11,8 @@ namespace RestSharpTest.Consts
     {
         public const string ExistingBoardId = "6377882b3f9e800060f41d29";
         public const string UserName = "vicky";
-        public const string Key = "5d3f254475ae9b62c9724b5237d40ea8";
-        public const string Token = "369733147d4c4d4f29f0fbb5e56307527de8c4576ea5320b79ed11bbb662e498";
+        public const string Key = "50000000000000ea8";
+        public const string Token = "300000000000000000000000000000000000000000000008";
         public const string BoardIdToUpdate = "637a3614c32e9d0029313742";
 
         public static readonly IEnumerable<Parameter> AuthQueryParams = new[]
